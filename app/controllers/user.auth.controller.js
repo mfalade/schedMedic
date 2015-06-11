76245967e4a09feb254221a3feee20f8cf6fb6c7
andela-mfalade <mayowa.falade@andela.co>
2015-06-11 22:48:53 +0100

    Change backend route base url

--- app/controllers/user.auth.controller.js
+++ app/controllers/user.auth.controller.js
@@ -36,8 +36,8 @@ var AuthController = {};
   };
 
   AuthController.middleware = function(req, res, next) {
-        // res.setHeader('Access-Control-Allow-Origin', 'https://schedmedic.herokuapp.com');
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5555');
+        res.setHeader('Access-Control-Allow-Origin', 'https://schedmedic.herokuapp.com');
+        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5555');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
