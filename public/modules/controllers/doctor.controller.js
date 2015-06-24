c44cb6872623b41b5fb241ad57e10281f7b258f6
andela-mfalade <mayowa.falade@andela.co>
2015-06-24 12:12:42 +0100

    Modify the doctor's home page

--- public/modules/controllers/doctor.controller.js
+++ public/modules/controllers/doctor.controller.js
@@ -1,7 +1,8 @@
 angular.module('doctorModule', ['scheduleServiceModule'])
   .controller('doctorCtrl', ['$scope','scheduleService', function($scope, scheduleService) {
     $scope.currentDoctorId = "557865621a81f3030020491f";
-    $scope.activeClass = 'doc_cancelled'; // This is to add the red background color by default
+    $scope.activeClass = 'doc_pending'; // This is to add the red background color by default
+    $scope.activeCategory = 'pending'; // This decides the text displayed above the schedule collection by default
     $scope.currentDoctorSchedules = {};
 
     $scope.getDoctorSchedules = function (doc_id) {
@@ -14,6 +15,7 @@ angular.module('doctorModule', ['scheduleServiceModule'])
 
     $scope.showCategory = function(param) {
       $scope.activeClass = 'doc_' + param;
+      $scope.activeCategory = param;
     };
 
   }]);
